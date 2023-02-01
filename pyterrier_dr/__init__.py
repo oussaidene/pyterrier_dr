82300759b8105d5966b70183ef6d5afb913df8c8
oussaidene <58385901+oussaidene@users.noreply.github.com>
2023-02-01 06:05:39 +0100

    Update __init__.py

--- pyterrier_dr/__init__.py
+++ pyterrier_dr/__init__.py
@@ -1,3 +1,4 @@
 from .indexes import DocnoFile, NilIndex, NumpyIndex, RankedLists, FaissFlat, FaissHnsw, MemIndex, TorchIndex
 from .tctcolbert_model import TctColBert
+from .custom_model import MyModel
 from .electra import ElectraScorer
