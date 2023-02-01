78b2a18dc23ed71b8371f831b53c2f7eada35a7c
oussaidene <58385901+oussaidene@users.noreply.github.com>
2023-02-01 06:22:12 +0100

    Update tctcolbert_model.py

--- pyterrier_dr/tctcolbert_model.py
+++ pyterrier_dr/tctcolbert_model.py
@@ -12,6 +12,7 @@ logger = ir_datasets.log.easy()
 
 class TctColBert(pt.Transformer):
     def __init__(self, model_name='castorini/tct_colbert-msmarco', batch_size=32, text_field='text', verbose=False, cuda=None):
+        print('HEYYYYYY')
         self.model_name = model_name
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.cuda = torch.cuda.is_available() if cuda is None else cuda
